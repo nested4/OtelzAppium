c0608b07c3b7aa95e5b9ad49b42754d9afa8b925
ihsan <111647146+nested4@users.noreply.github.com>
2023-05-05 14:21:58 +0300

    APK file path corrected

--- src/test/java/com/otelz/tests/OtelReservationUpdated.java
+++ src/test/java/com/otelz/tests/OtelReservationUpdated.java
@@ -15,12 +15,11 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
-public class OtelzReservation {
+public class OtelReservationUpdated {
     AppiumDriver<MobileElement> driver;
     WebDriverWait wait;
 
@@ -32,8 +31,9 @@ public class OtelzReservation {
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 5");
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.0");
-        caps.setCapability(MobileCapabilityType.APP, new File("./src/test/resources/Otelz-dev.apk")
-                .getAbsolutePath());
+        caps.setCapability(MobileCapabilityType.APP, "C:\\Users\\ozgei\\IdeaProjects\\OtelzAppium\\src" +
+                "\\test\\resources\\Otelz-dev.apk");
+
         //set URL for the appium server
         URL url = new URL("http://localhost:4723/wd/hub");
 
@@ -51,40 +51,45 @@ public class OtelzReservation {
 
         //2.+	Otel sekmesinde "Ankara" girilir ve ilk seçenek seçilir
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/search_src_text"))).sendKeys("Ankara");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/search_src_text"))).
+                sendKeys("Ankara");
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//*[@resource-id=\"com.otelz.mobil.search:id/tvSearchedItem\"])[1]"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("" +
+                "(//*[@resource-id=\"com.otelz.mobil.search:id/tvSearchedItem\"])[1]"))).click();
 
         //3.+	Tarih sekmesi açılır ve gidiş tarihi 3, dönüş için 5 gün ilerisi seçilir ve arama butonuna basılır.
-
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/tv_start_date"))).click();
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@resource-id=\"com.otelz.mobil:id/tv_day_number\" and @text=\"8\"]"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.
+                xpath("//*[@resource-id=\"com.otelz.mobil:id/tv_day_number\" and @text=\"8\"]"))).click();
 
-        driver.findElement(By.xpath("//*[@resource-id=\"com.otelz.mobil:id/tv_day_number\" and @text=\"10\"]")).click();
+        driver.findElement(By.xpath("//*[@resource-id=\"com.otelz.mobil:id/tv_day_number\" and @text=\"10\"]"))
+                .click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@resource-id=\"com.otelz.mobil:" +
                 "id/tvCheckDaysAndContinueDates\"]"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/view_search_background"))).click();
 
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/view_search_background"))).click();
         //4.+	İlk çıkan tesisin isim ve fiyat bilgisi alınır ve tesis sayfası açılır
 
-        System.out.println("Be patient, Test will NOT fail. You are waiting because of a slow server");
+        System.out.println("Be patient, Test will NOT fail. You are waiting because of a BUSY server");
 
         String expectedHotelName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("" +
                 "//*[@resource-id='com.otelz.mobil:id/textView21']"))).getAttribute("text");
         System.out.println("expectedHotelName = " + expectedHotelName);
 
-        System.out.println("Be patient, Test will NOT fail. You are waiting because of a slow server");
+        System.out.println("Be patient, Test will NOT fail. You are waiting because of a BUSY server");
 
-        String expectedHotelPrice = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("" +
-                "//*[@resource-id='com.otelz.mobil:id/tvFinalPrice']"))).getAttribute("text");
+        String expectedHotelPrice = driver.findElement(By.xpath("" +
+                "//*[@resource-id='com.otelz.mobil:id/tvFinalPrice']")).getAttribute("text");
         System.out.println("expectedHotelPrice = " + expectedHotelPrice);
 
-        System.out.println("Be patient, Test will NOT fail. You are waiting because of a slow server");
+        System.out.println("Be patient, Test will NOT fail. You are waiting because of a BUSY server");
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("" +
-                "//*[@resource-id='com.otelz.mobil:id/textView21']"))).click();
+        driver.findElement(By.xpath("" +
+                "//*[@resource-id='com.otelz.mobil:id/textView21']")).click();
+
         //5.+	Tesis sayfasında tesisin ismi ve fiyatı doğrulanır
 
         String actualHotelName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/" +
@@ -97,8 +102,8 @@ public class OtelzReservation {
                 "the expected string");
         Assertions.assertTrue(actualHotelPrice.contains(expectedHotelPrice.substring(2)), "The actual " +
                 "string does not contain the expected string");
-        //6.+	"Diğer Oda Seçenekleri" butonuna basılır ve bir oda seçilir
 
+        //6.+	"Diğer Oda Seçenekleri" butonuna basılır ve bir oda seçilir
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/btnChooseRoom"))).click();
 
         swipeV(0.7, 0.3);
@@ -106,7 +111,6 @@ public class OtelzReservation {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/btnMakeReservation"))).click();
 
         //7.+	Rezervasyon 1. sayfasında kişi bilgileri girilir ve "Sonraki Adım" butonuna tıklanır
-
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/tvPersonalInfoName")))
                 .sendKeys("John");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/tvPersonalInfoSurname")))
@@ -119,7 +123,8 @@ public class OtelzReservation {
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@text='Turkey - 90']"))).click();
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/tvPersonalInfoPhone"))).sendKeys("5555555555");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/tvPersonalInfoPhone"))).
+                sendKeys("5555555555");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/tvPersonalInfoCountry"))).click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@text='Turkey']"))).click();
@@ -133,7 +138,8 @@ public class OtelzReservation {
 
         //8.	Rezervasyon 2. sayfasında kupon alanı açılır ve "APP05" girilir, kullan butonuna tıklanır ve uyarı mesajı geldiği doğrulanır.
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil.reservationSteps:id/tvCouponUse"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil.reservationSteps:id/etCouponCode"))).sendKeys("APP05");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil.reservationSteps:id/etCouponCode"))).
+                sendKeys("APP05");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil.reservationSteps:id/button4"))).click();
         WebElement alertMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("android:id/message")));
         Assertions.assertTrue(alertMessage.isDisplayed());
@@ -178,7 +184,6 @@ public class OtelzReservation {
                 .release()
                 .perform();
     }
-
 }
 /*Android App Otomasyonu
 Android apps otomasyonu için Java ile Maven projesi oluşturulmalı, TestNG(veya Junit), Appium kullanılması gerekmektedir.
--- src/test/java/com/otelz/tests/OtelzBooking.java
+++ src/test/java/com/otelz/tests/OtelzBooking.java
@@ -15,12 +15,11 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
-public class OtelzReservation {
+public class OtelzBooking {
     AppiumDriver<MobileElement> driver;
     WebDriverWait wait;
 
@@ -32,16 +31,14 @@ public class OtelzReservation {
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 5");
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.0");
-        caps.setCapability(MobileCapabilityType.APP, new File("./src/test/resources/Otelz-dev.apk")
-                .getAbsolutePath());
+        caps.setCapability(MobileCapabilityType.APP, "C:\\Users\\ozgei\\IdeaProjects\\OtelzAppium\\src\\test\\resources\\Otelz-dev.apk");
+
         //set URL for the appium server
         URL url = new URL("http://localhost:4723/wd/hub");
 
         //launch appiumDriver
         driver = new AndroidDriver<MobileElement>(url, caps);
         wait = new WebDriverWait(driver, 30);
-
-        //1.+	Uygulama başlatılır ve tanıtım sayfalarında 2 kere sağa kaydırılır ve devam butonuna tıklanır
         swipeH(0.7, 0.3);
         swipeH(0.7, 0.3);
         driver.findElement(By.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")).click();
@@ -49,43 +46,62 @@ public class OtelzReservation {
         driver.findElement(By.id("com.otelz.mobil:id/materialButtonContinue")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/tv_search_label"))).click();
 
-        //2.+	Otel sekmesinde "Ankara" girilir ve ilk seçenek seçilir
-
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/search_src_text"))).sendKeys("Ankara");
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//*[@resource-id=\"com.otelz.mobil.search:id/tvSearchedItem\"])[1]"))).click();
 
-        //3.+	Tarih sekmesi açılır ve gidiş tarihi 3, dönüş için 5 gün ilerisi seçilir ve arama butonuna basılır.
-
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/tv_start_date"))).click();
 
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout" +
+//                "/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout" +
+//                "/android.view.ViewGroup/android.widget.RelativeLayout[1]/androidx.recyclerview.widget.RecyclerView/android." +
+//                "widget.LinearLayout[1]/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget." +
+//                "LinearLayout[8]/android.widget.TextView"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@resource-id=\"com.otelz.mobil:id/tv_day_number\" and @text=\"8\"]"))).click();
 
+//        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android." +
+//                "widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android." +
+//                "widget.RelativeLayout[1]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android." +
+//                "widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[10]/android.widget." +
+//                "TextView")).click();
         driver.findElement(By.xpath("//*[@resource-id=\"com.otelz.mobil:id/tv_day_number\" and @text=\"10\"]")).click();
 
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/" +
+//                "android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/" +
+//                "android.view.ViewGroup/android.widget.RelativeLayout[1]/androidx.recyclerview.widget.RecyclerView/android.widget." +
+//                "LinearLayout[2]"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@resource-id=\"com.otelz.mobil:" +
                 "id/tvCheckDaysAndContinueDates\"]"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/view_search_background"))).click();
 
-        //4.+	İlk çıkan tesisin isim ve fiyat bilgisi alınır ve tesis sayfası açılır
-
         System.out.println("Be patient, Test will NOT fail. You are waiting because of a slow server");
-
+//        String expectedHotelName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/" +
+//                "android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/" +
+//                "android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/" +
+//                "android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TextView[1]"))).getAttribute("text");
+//
         String expectedHotelName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("" +
                 "//*[@resource-id='com.otelz.mobil:id/textView21']"))).getAttribute("text");
         System.out.println("expectedHotelName = " + expectedHotelName);
 
         System.out.println("Be patient, Test will NOT fail. You are waiting because of a slow server");
-
+//        String expectedHotelPrice = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/" +
+//                "android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/" +
+//                "android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/" +
+//                "androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.view.ViewGroup/" +
+//                "android.widget.RelativeLayout/android.widget.TextView[3]"))).getAttribute("text");
         String expectedHotelPrice = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("" +
                 "//*[@resource-id='com.otelz.mobil:id/tvFinalPrice']"))).getAttribute("text");
         System.out.println("expectedHotelPrice = " + expectedHotelPrice);
 
         System.out.println("Be patient, Test will NOT fail. You are waiting because of a slow server");
-
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/" +
+//                "android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/" +
+//                "android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/" +
+//                "androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.view.ViewGroup/" +
+//                "android.widget.RelativeLayout/android.widget.TextView[3]"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("" +
                 "//*[@resource-id='com.otelz.mobil:id/textView21']"))).click();
-        //5.+	Tesis sayfasında tesisin ismi ve fiyatı doğrulanır
 
         String actualHotelName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/" +
                 "tvFacilityDetailName"))).getAttribute("text");
@@ -97,15 +113,11 @@ public class OtelzReservation {
                 "the expected string");
         Assertions.assertTrue(actualHotelPrice.contains(expectedHotelPrice.substring(2)), "The actual " +
                 "string does not contain the expected string");
-        //6.+	"Diğer Oda Seçenekleri" butonuna basılır ve bir oda seçilir
-
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/btnChooseRoom"))).click();
 
         swipeV(0.7, 0.3);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/tvPersonCount"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/btnMakeReservation"))).click();
-
-        //7.+	Rezervasyon 1. sayfasında kişi bilgileri girilir ve "Sonraki Adım" butonuna tıklanır
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/tvPersonalInfoName")))
                 .sendKeys("John");
@@ -116,7 +128,10 @@ public class OtelzReservation {
 
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/etPersonalInfoPhoneCode"))).click();
-
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/" +
+//                "android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/" +
+//                "android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget." +
+//                "RecyclerView/android.widget.RelativeLayout[1]/android.widget.TextView"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@text='Turkey - 90']"))).click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/tvPersonalInfoPhone"))).sendKeys("5555555555");
@@ -131,15 +146,18 @@ public class OtelzReservation {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/checkBoxPermission"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil:id/btnPersonalInfoNext2"))).click();
 
-        //8.	Rezervasyon 2. sayfasında kupon alanı açılır ve "APP05" girilir, kullan butonuna tıklanır ve uyarı mesajı geldiği doğrulanır.
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil.reservationSteps:id/tvCouponUse"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil.reservationSteps:id/etCouponCode"))).sendKeys("APP05");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.otelz.mobil.reservationSteps:id/button4"))).click();
         WebElement alertMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("android:id/message")));
         Assertions.assertTrue(alertMessage.isDisplayed());
+        //        MobileElement clearElem = driver.findElement(MobileBy.AccessibilityId("clear"));
 
+//        System.out.println("Text of elem: " + clearElem.getText());
+//        Assertions.assertTrue(clearElem.isDisplayed());
 
-        driver.closeApp();
+        //close the app
+//        driver.closeApp();
 
     }
 
@@ -178,7 +196,6 @@ public class OtelzReservation {
                 .release()
                 .perform();
     }
-
 }
 /*Android App Otomasyonu
 Android apps otomasyonu için Java ile Maven projesi oluşturulmalı, TestNG(veya Junit), Appium kullanılması gerekmektedir.
@@ -189,5 +206,5 @@ Android apps otomasyonu için Java ile Maven projesi oluşturulmalı, TestNG(vey
 5.+	Tesis sayfasında tesisin ismi ve fiyatı doğrulanır
 6.+	"Diğer Oda Seçenekleri" butonuna basılır ve bir oda seçilir
 7.+	Rezervasyon 1. sayfasında kişi bilgileri girilir ve "Sonraki Adım" butonuna tıklanır
-8.	Rezervasyon 2. sayfasında kupon alanı açılır ve "APP05" girilir, kullan butonuna tıklanır ve uyarı mesajı geldiği doğrulanır.
+8.+	Rezervasyon 2. sayfasında kupon alanı açılır ve "APP05" girilir, kullan butonuna tıklanır ve uyarı mesajı geldiği doğrulanır.
 */
